714e742002cf147065ee5c99a37719b27965f069
pl4netary <117487576+Logobot3000@users.noreply.github.com>
2025-10-27 09:09:34 -0400

    fixed logo and added transition
    yes

--- scripts/ui/startup_menu.gd
+++ scripts/ui/startup_menu.gd
@@ -1,9 +1,19 @@
 extends Node
 
 @onready var tiles: TileMapLayer = $InteractableTiles;
-var hue: int = 125;
+@onready var white_flash: TextureRect = $WhiteFlash;
+var hue: float = 125.0;
+
+
+func _ready() -> void:
+	white_flash.visible = true;
+	await get_tree().create_timer(0.5).timeout;
+	var tween: Tween = get_tree().create_tween();
+	tween.tween_property(white_flash, "modulate", Color("#FFFFFF", 1.0), 0.75);
+	tween.tween_property(white_flash, "modulate", Color("#FFFFFF", 0.0), 3);
+
 
 func _process(delta: float) -> void:
-	hue += 1;
-	tiles.modulate = Color.from_hsv((hue % 360) / 360.0, 0.15, 1.0);
+	hue += 0.5;
+	tiles.modulate = Color.from_hsv((int(floor(hue)) % 360) / 360.0, 0.25, 1.0);
 	tiles.rotation += 0.005;
