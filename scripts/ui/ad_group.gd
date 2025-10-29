9908342cd1fd7876b4081bfdf19172797dfcecfe
Logobot3000 <kroll.logan@gmail.com>
2025-10-28 23:05:06 -0400

    made the scuffed menu less scuffed and added join lobby and host lobby buttons to add later

--- scripts/ui/ad_group.gd
+++ scripts/ui/ad_group.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 
 ## Does the little ad popping up animation.
 func popup(n: Node, index: int) -> void:
-	await get_tree().create_timer(randf_range(3.0 * index, 8.0 * index) + 3).timeout;
-	var tween: Tween = get_tree().create_tween();
-	tween.tween_property(n, "scale", Vector2(2, 2), 0.1);
+	if get_tree() != null:
+		await get_tree().create_timer(randf_range(3.0 * index, 8.0 * index) + 3).timeout;
+		var tween: Tween = get_tree().create_tween();
+		tween.tween_property(n, "scale", Vector2(2, 2), 0.1);
