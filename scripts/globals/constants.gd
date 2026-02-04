b778ac09c3df18ef4556eaebb9a0688a93d199ce
Logobot3000 <kroll.logan@gmail.com>
2026-02-03 20:16:02 -0500

    the appid update tm

--- scripts/globals/constants.gd
+++ scripts/globals/constants.gd
@@ -3,8 +3,8 @@ extends Node;
 ## A constant for limiting gravity acceleration in gravity equations. Used in VelocityComponent, but there may be other uses.
 var GRAV_CONSTANT: float = 0.3625;
 
-## The AppID for the game on Steam. The default is 480. Replace this once the game actually has a Steam page.
-var APP_ID: String = "480";
+## The AppID for the game on Steam. YAYYYY ITS ON STEAMMMMM
+var APP_ID: String = "4398340"; 
 
 ## The packet read limit. This is how many packets of data a server can read at a time.
 var PACKET_READ_LIMIT: int = 32;
